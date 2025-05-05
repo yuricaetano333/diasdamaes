b45b2e27f3268b2ba351ca3adc7d0b44b9776d32
yuricaetano333 <yuri89457@gmail.com>
2025-05-05 13:08:25 -0300

    primeirocommities

---
+++ dias da mae yuri/code mae.py
@@ -0,0 +1,31 @@
+import time
+
+mensagem =[
+    "Feliz dia das mães!",
+"Você é a minha maior inspiração!",
+"Obrigado pela sua dedicação e cuidado!",
+"Minha primeira amiga e até hoje a melhor companhia!",
+"Te amo infinitamente",
+"❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+"👑👑👑👑👑👑👑👑",
+"as melhores risadas sempre contigo mãe!",
+"obrigado por todos ensinamentos",
+]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5) #espere 1.5 segundos antes de mostrar a próxima linha
+
+    # criando um pequeno coração com caracteres
+    print()
+    print("    **    **")
+    print("  ****** ******")
+    print(" **************")
+    print("  *************")
+    print("   **********")
+    print("     ******")
+    print("      ****")
+    print("       **")
+
+    
+
